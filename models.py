cb72a6e1c8b7bf272e03889a14bd40f6ec608856
DTaylorMedia <36168276+DTaylorMedia@users.noreply.github.com>
2024-06-28 19:57:07 +0100

    Tasks now have timestamps for when they're created/modified

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,3 +7,5 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	task = db.Column(db.String(80), unique = True)
 	complete = db.Column(db.Boolean, default=False)
+	date_created = db.Column(db.DateTime, default=datetime.now())
+	date_modified = db.Column(db.DateTime, default=datetime.now())
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template,request,redirect,url_for
+from datetime import datetime
 from models import db,Todo
 
 view = Blueprint('view', __name__)
@@ -19,6 +20,15 @@ def add():
 def update(todo_id):
 	todo = Todo.query.filter_by(id=todo_id).first()
 	todo.complete = not todo.complete
+	todo.date_modified = datetime.now()
+
+	# print(f"{todo.date_created} --- {todo.date_created.timestamp()}")
+	# print(f"{todo.date_modified} --- {todo.date_modified.timestamp()}")
+	# if todo.date_modified > todo.date_created:
+	# 	print("> True!")
+	# else:
+	# 	print("> False")
+
 	db.session.commit()
 	return redirect(url_for('view.home'))
 
@@ -32,3 +42,4 @@ def delete(todo_id):
 @view.route("/page2")
 def page():
 	return render_template('page2.html')
+
